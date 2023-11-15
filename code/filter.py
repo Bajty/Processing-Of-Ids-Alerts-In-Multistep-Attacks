65c77380ec3aef8c79875a1971ffeb0d00039a82
Frantisek Kurimsky <feri.kurimsky@gmail.com>
2023-11-15 16:18:25 +0100

    update to one tunconfig for dataset

--- code/filter.py
+++ code/filter.py
@@ -1,5 +1,16 @@
 import pandas as pd
-import numpy as np
+from constants import (
+    PERC_DIFF_MIN,
+    PERC_DIFF_MAX,
+    COUNT_MIN,
+    COUNT_MAX_HOURS
+)
+from tasks import (
+    get_counts_days,
+    get_stats,
+    get_counts_hours_sub_mean,
+    get_counts_hours
+)
 
 
 def filter_all_days_stats(data, conf):
@@ -9,3 +20,49 @@ def filter_all_days_stats(data, conf):
 def filter_perc_count(data, conf):
     cond_filter = data[((data['perc_diff'] < conf['perc_diff']) & (data['count'] >= conf['count']))]
     return pd.concat([data[(data['max'] == 0)], cond_filter])
+
+
+def filter_all_days(data, all_data_filtered, filter, paths):
+    if PERC_DIFF_MIN < filter['perc_diff'] < PERC_DIFF_MAX and \
+            COUNT_MIN < filter['count'] <= len(paths) and \
+            type(filter['count']) == int and type(filter['perc_diff']) == float:
+        group_cols = ["sid", "rule_name", "file_path"]
+        stats = get_counts_days(data=data, group_cols=group_cols)
+        col = stats[paths]
+        stats = get_stats(stats, col)
+        stats.reset_index(inplace=True)
+        filtered_stats = filter_all_days_stats(stats, filter)
+        filter_list = filtered_stats['sid'].unique().tolist()
+        # Filter data based on all days stats
+        return all_data_filtered[~all_data_filtered.set_index('sid').index.isin(filter_list)]
+    else:
+        raise Exception('Wrong value for perc_dif and count')
+
+
+def filter_count_hours(data, all_data_filtered, filter, benign, inputs):
+    if PERC_DIFF_MIN < filter['perc_diff'] <= PERC_DIFF_MAX and \
+            COUNT_MIN < filter['count'] <= COUNT_MAX_HOURS and \
+            type(filter['count']) == int and type(filter['perc_diff']) == float:
+        group_cols = ["file_path", "sid"] + [filter['addr']] + ["hour"]
+        if filter['sub_mean'] is True:
+            if filter['mean_from'] == 'benign_days':
+                calculate_from = benign
+            elif filter['mean_from'] == 'all_days':
+                calculate_from = inputs + benign
+            else:
+                raise Exception('WRONG OPTION FOR mean_from')
+
+            stats = get_counts_hours_sub_mean(data, group_cols=group_cols, inputs=calculate_from, paths=inputs + benign)
+        elif filter['sub_mean'] is False:
+            stats = get_counts_hours(data=data, group_cols=group_cols, paths=inputs + benign)
+        else:
+            raise Exception('WRONG OPTION FOR sub_mean')
+        col = stats.loc[:, '0':'23']
+        stats = get_stats(stats, col)
+        stats.reset_index(inplace=True)
+        filtered_stats = filter_perc_count(stats, filter)
+        filter_list = set(list(filtered_stats[group_cols[:-1]].itertuples(index=False, name=None)))
+        return all_data_filtered[
+            ~all_data_filtered.set_index(group_cols[:-1]).index.isin(filter_list)]
+    else:
+        raise Exception('Wrong value for perc_dif and count')
--- code/main.py
+++ code/main.py
@@ -1,26 +1,25 @@
 # IMPORTS
 import argparse
-import pandas as pd
+import os
 from tasks import (
     load_config,
     load_dataset,
-    get_file_paths,
-    get_stats,
-    get_counts_days,
-    get_counts_hours,
-    get_counts_hours_sub_mean,
-    check_filters,
     export_agg_data,
     export_filtered_data,
     load_classes
 )
 from filter import (
-    filter_all_days_stats,
-    filter_perc_count
+    filter_all_days,
+    filter_count_hours
 )
 from aggregation import (
     create_events,
     aggregtion
+)
+from constants import (
+    INPUTS_PATH,
+    BENIGN_INPUTS,
+    CLASSES
 )
 
 # ARGPARSER for configuration file
@@ -36,91 +35,47 @@ print(f"LOADING === config dataset {config['dataset_name']}")
 # LOAD specs file for dataset
 print(f"DEFINING headers, allowed filters, input files, input benign files for {config['dataset_name']}")
 dataset_path = config['dataset_path']
-dataset_config = load_config(f'{dataset_path}config.json')
+DATASET_INPUT_FILES = [INPUTS_PATH + '/' + x for x in os.listdir(f'{dataset_path}/{INPUTS_PATH}/')]
+DATASET_BENIGN_FILES = [BENIGN_INPUTS + '/' + x for x in os.listdir(f'{dataset_path}/{BENIGN_INPUTS}/')]
+DATASET_CLASSES = [CLASSES + '/' + x for x in os.listdir(f'{dataset_path}/{CLASSES}/')]
 
 # LOAD dataset
 data = load_dataset(
-    dataset_path=dataset_path, inputs=dataset_config['inputs'],
-    inputs_benign=dataset_config['inputs_benign'] if 'inputs_benign' in dataset_config else [],
-    headers=dataset_config['headers'],
-    timedelta=dataset_config['timedelta'] if 'timedelta' in dataset_config else False,
-    timedelta_function=dataset_config['timedelta_function'] if 'timedelta_function' in dataset_config else '-'
+    dataset_path=dataset_path,
+    inputs=DATASET_INPUT_FILES,
+    inputs_benign=DATASET_BENIGN_FILES,
+    timedelta=config['timedelta'] if 'timedelta' in config else False,
+    timedelta_function=config['timedelta_function'] if 'timedelta_function' in config else '-'
 )
 
-# DEFINE unique paths, file_path represents in dataset value for each row from which file is specific row loaded
-# basically this is the same as inputs benign and inputs from defined dataset
-paths = get_file_paths(data=data)
+all_data_filtered = data.copy()
+print(f"Number of loaded events: {len(all_data_filtered)}")
 
-# CHECK if all filters are correctly defined for this run
-if check_filters(config['filters'], dataset_config['filters']):
+# APPLY all defined filter from config file
+for index, filter in enumerate(config['filters'] if config['filters'] is not None else []):
+    if filter['type'] == 'all_days':
+        all_data_filtered = filter_all_days(data, all_data_filtered, filter,
+                                            paths=DATASET_INPUT_FILES + DATASET_BENIGN_FILES)
+    elif filter['type'] == 'count_hours':
+        all_data_filtered = filter_count_hours(data, all_data_filtered, filter, benign=DATASET_BENIGN_FILES,
+                                               inputs=DATASET_INPUT_FILES)
 
-    all_data_filtered = data.copy()
-    print(f"Number of loaded events: {len(all_data_filtered)}")
+    print(f"Number of events after {index} filter : {len(all_data_filtered)}")
 
-    # APPLY all defined filter from config file
-    for index, filter in enumerate(config['filters'] if config['filters'] is not None else []):
-        if filter['type'] == 'all_days':
-            if dataset_config['filters']['all_days']['perc_diff_min'] < filter['perc_diff'] <= \
-                    dataset_config['filters']['all_days']['perc_diff_max'] and \
-                    dataset_config['filters']['all_days']['count_min'] < filter['count'] <= \
-                    len(dataset_config['inputs_benign'] + dataset_config['inputs']):
+# sort filtered data according
+all_data_filtered = all_data_filtered.sort_values(by=['datetime'])
 
-                group_cols = dataset_config['filters'][filter['type']]['group_cols']
-                stats = get_counts_days(data=data, group_cols=group_cols)
-                col = stats[paths]
-                stats = get_stats(stats, col)
-                stats.reset_index(inplace=True)
-                filtered_stats = filter_all_days_stats(stats, filter)
-                filter_list = filtered_stats['sid'].unique().tolist()
-                # Filter data based on all days stats
-                all_data_filtered = all_data_filtered[~all_data_filtered.set_index('sid').index.isin(filter_list)]
-            else:
-                raise Exception('Wrong value for perc_dif and count')
-        elif filter['type'] == 'count_hours':
-            if dataset_config['filters']['count_hours']['perc_diff_min'] < filter['perc_diff'] <= \
-                    dataset_config['filters']['count_hours']['perc_diff_max'] and \
-                    dataset_config['filters']['count_hours']['count_min'] < filter['count'] <= \
-                    dataset_config['filters']['count_hours']['count_max']:
-                if filter['group_cols'] not in dataset_config['filters'][filter['type']]['group_cols_options']:
-                    raise Exception('wrong input for group_cols option in filter options')
-                group_cols = filter['group_cols']
-                if filter['sub_mean'] is True:
-                    if filter['mean_from'] not in dataset_config['filters'][filter['type']]['from_mean_options']:
-                        raise Exception('wrong input for mean_from option in filter options')
-                    if filter['mean_from'] == 'benign_days':
-                        inputs = dataset_config['inputs_benign']
-                    else:
-                        inputs = dataset_config['inputs']
+# EXPORT filtered events into json
+if config['filters'] is not None:
+    export_filtered_data(all_data_filtered, name=config['dataset_name'])
 
-                    stats = get_counts_hours_sub_mean(data, group_cols=group_cols, inputs=inputs)
-                else:
-                    stats = get_counts_hours(data=data, group_cols=group_cols)
-                col = stats.loc[:, '0':'23']
-                stats = get_stats(stats, col)
-                stats.reset_index(inplace=True)
-                filtered_stats = filter_perc_count(stats, filter)
-                filter_list = set(list(filtered_stats[group_cols[:-1]].itertuples(index=False, name=None)))
-                all_data_filtered = all_data_filtered[
-                    ~all_data_filtered.set_index(group_cols[:-1]).index.isin(filter_list)]
-            else:
-                raise Exception('Wrong value for perc_dif and count')
+# CREATE classes for aggregation
+all_filtered_events__class = create_events(filtered_data=all_data_filtered)
 
-        print(f"Number of events after {index} filter : {len(all_data_filtered)}")
+# RUN aggregation over moving window
+classes = load_classes(dataset_path=dataset_path, path=DATASET_CLASSES[0])
+agg_events = aggregtion(all_filtered_events=all_filtered_events__class, delta=config['agg_delta'], classes=classes)
+print(f"Number of aggregated events: {len(agg_events)}")
 
-    # sort filtered data according
-    all_data_filtered = all_data_filtered.sort_values(by=['datetime'])
-
-    # EXPORT filtered events into json
-    if config['filters'] is not None:
-        export_filtered_data(all_data_filtered, name=config['dataset_name'])
-
-    # CREATE classes for aggregation
-    all_filtered_events__class = create_events(filtered_data=all_data_filtered)
-
-    # RUN aggregation over moving window
-    classes = load_classes(dataset_path=dataset_path, path=dataset_config['classes'])
-    agg_events = aggregtion(all_filtered_events=all_filtered_events__class, delta=config['agg_delta'], classes=classes)
-    print(f"Number of aggregated events: {len(agg_events)}")
-
-    # EXPORT aggregated events into json
-    export_agg_data(agg_events=agg_events, name=config['dataset_name'])
+# EXPORT aggregated events into json
+export_agg_data(agg_events=agg_events, name=config['dataset_name'])
